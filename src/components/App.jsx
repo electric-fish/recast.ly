bd366a8cc9965330407c750d646871b80d778202
elchangtw <elchang.tw@gmail.com>
2019-11-05 01:17:39 -0800

    TypeError: props.onClick is not a function in VideoListEntry.jsx:20

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,25 +3,70 @@ import VideoPlayer from './VideoPlayer.js';
 import VideoList from './VideoList.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 
-var App = (props) => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
-      </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        {/* <div><h5><em>videoPlayer</em> view goes here</h5></div> */}
-        <div><VideoPlayer video={exampleVideoData[0]} /></div>
-      </div>
-      <div className="col-md-5">
-        {/* <div><h5><em>videoList</em> view goes here</h5></div> */}
-        <div><VideoList videos={exampleVideoData} /></div>
+// var App = (props) => (
+//   <div>
+//     <nav className="navbar">
+//       <div className="col-md-6 offset-md-3">
+//         <div><h5><em>search</em> view goes here</h5></div>
+//       </div>
+//     </nav>
+//     <div className="row">
+//       <div className="col-md-7">
+//         {/* <div><h5><em>videoPlayer</em> view goes here</h5></div> */}
+//         <div><VideoPlayer video={exampleVideoData[0]} /></div>
+//       </div>
+//       <div className="col-md-5">
+//         {/* <div><h5><em>videoList</em> view goes here</h5></div> */}
+//         <div><VideoList videos={exampleVideoData} /></div>
+//       </div>
+//     </div>
+//   </div>
+// );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // videos: {exampleVideoData},
+      // currentVideo: {exampleVideoData[0]}
+      // videos: exampleVideoData,
+      currentVideo: exampleVideoData[0]
+    };
+
+    this.setNewVideo = this.setNewVideo.bind(this);
+  }
+
+  setNewVideo(video) {
+    console.log('video event happening');
+    this.setState(state => ({
+      currentVideo: this.state.currentVideo = video;
+    }));
+  }
+
+  render() {
+    return (
+    <div>
+      <nav className="navbar">
+        <div className="col-md-6 offset-md-3">
+          <div><h5><em>search</em> view goes here</h5></div>
+        </div>
+      </nav>
+      <div className="row">
+        <div className="col-md-7">
+          <div><VideoPlayer video={this.state.currentVideo} /></div>
+        </div>
+        <div className="col-md-5">
+          {/* <div><h5><em>videoList</em> view goes here</h5></div> */}
+          <div><VideoList onClick={this.setNewVideo} videos={this.props.videos} /></div>
+        </div>
       </div>
     </div>
-  </div>
-);
+
+    );
+  }
+}
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -2,11 +2,6 @@ import VideoListEntry from './VideoListEntry.js';
 
 var VideoList = (props) => (
   <div className="video-list">
-    {/* <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div> */}
 
     {/* <div><VideoListEntry video={props.videos[0]} /></div>
     <div><VideoListEntry video={props.videos[1]} /></div>
@@ -14,15 +9,16 @@ var VideoList = (props) => (
     <div><VideoListEntry video={props.videos[3]} /></div>
     <div><VideoListEntry video={props.videos[4]} /></div> */}
 
-    {props.videos.map((video) => (
+    {/* {props.videos.map((video) => (
       <VideoListEntry video={video} />
-    ))}
+    ))} */}
 
-    {/* <div><h5><em>videoListEntry</em> {props.videos[0]}</h5></div>
-    <div><h5><em>videoListEntry</em> {props.videos[1]}</h5></div>
-    <div><h5><em>videoListEntry</em> {props.videos[2]}</h5></div>
-    <div><h5><em>videoListEntry</em> {props.videos[3]}</h5></div>
-    <div><h5><em>videoListEntry</em> {props.videos[4]}</h5></div> */}
+    {props.videos.map((video) => {
+      return (
+        <VideoListEntry onClick={props.onClick} video={video} />
+      );
+    })}
+
   </div>
 );
 
